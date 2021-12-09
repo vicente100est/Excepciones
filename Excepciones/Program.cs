669a90c42fc5232fe6b36c0d013b074055e5bf30
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2021-12-08 19:11:04 -0600

    Curso de HDELEON C#

---
+++ Excepciones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Users\vicen\OneDrive\Documentos\x.txt");
+                Console.WriteLine(content);
+
+                string content2 = File.ReadAllText(@"C:\Users\vicen\OneDrive\Documentos\x2.txt");
+                Console.WriteLine(content2);
+
+                throw new Exception("Ocurrio algo raro");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("El archivo no existe");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Aquí me he ejecutado pase lo que pase");
+            }
+
+            Console.WriteLine("Aquí se sigue ejecutando");
+        }
+    }
+}
